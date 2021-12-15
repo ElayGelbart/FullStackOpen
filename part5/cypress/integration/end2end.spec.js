3c887311fe06459bc51aa8749f8fff4d3a1a29b3
ElayGelbart <elaygelbart@gmail.com>
2021-12-15 12:36:57 +0200

    added cypress

---
+++ part5/cypress/integration/end2end.spec.js
@@ -0,0 +1,12 @@
+// end2end.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Check Test', () => {
+  it('should be true', () => {
+    expect(true).to.equal(true)
+  });
+});
